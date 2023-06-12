b41e64cfef1d3d13e0af60a13d7ea43060dc53f9
njohnhub <njohn4752@gmail.com>
2023-06-12 22:03:55 +0300

    client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import "./App.css";
+import { createBrowserRouter,
+   RouterProvider, 
+   Route } from "react-router-dom";
 
+
+   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello</div>
+    }
+   ])
 
 function App() {
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
